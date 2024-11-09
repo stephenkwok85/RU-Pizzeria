5b5917ee1077db217aa6d74b77dc3556c1128d69
Stephen Kwok <59847958+stephenkwok85@users.noreply.github.com>
2024-11-09 16:36:03 -0500

    Update OrderManager.java

--- src/main/java/com/example/ru_pizzeria/OrderManager.java
+++ src/main/java/com/example/ru_pizzeria/OrderManager.java
@@ -1,10 +1,26 @@
 package com.example.ru_pizzeria;
 
+import pizzeria_package.Pizza;
+import java.util.HashMap;
+import java.util.Map;
+
 public class OrderManager {
+    private static final Map<Integer, Pizza> orders = new HashMap<>();
+    private static int nextOrderNumber = 1;
 
-    private static int orderCounter = 1;
+    // Method to add an order
+    public static void addOrder(Pizza pizza) {
+        orders.put(nextOrderNumber, pizza);
+        nextOrderNumber++;
+    }
 
+    // Method to get an order by order number
+    public static Pizza getOrder(int orderNumber) {
+        return orders.get(orderNumber);
+    }
+
+    // Method to get the next order number
     public static int getNextOrderNumber() {
-        return orderCounter++;
+        return nextOrderNumber;
     }
 }
